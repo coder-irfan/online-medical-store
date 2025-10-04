ffb41563c68a52c9021bf3fe066afafd3d6f628f
Mohammad Irfan <irfan.noorzada123@gmail.com>
2025-10-04 15:44:28 +0430

    routes change

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -11,4 +11,22 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Update images from svg to webp
+app.put("/change-images", async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    for (let p of products) {
+      if (p.image) {
+        p.image = p.image.replace(/\.jpg|\.svg/g, ".webp");
+        await p.save();
+      }
+    }
+
+    res.status(200).json({ message: "All images updated to webp!" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to change images!" });
+  }
+});
+
 module.exports = router;
